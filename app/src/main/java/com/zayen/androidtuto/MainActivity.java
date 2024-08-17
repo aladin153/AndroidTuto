1dc6359e566395fe21c4f539214a71fa4aabccc1
Alaeddine ZAYEN <alaedine.zayen@gmail.com>
2024-08-18 00:27:51 +0200

    Toast
    Signed-off-by: Alaeddine ZAYEN <alaedine.zayen@gmail.com>

--- app/src/main/java/com/zayen/androidtuto/MainActivity.java
+++ app/src/main/java/com/zayen/androidtuto/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -30,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Log.d(TAG, "Button Clicked");
+                Toast.makeText(MainActivity.this, "This this a Toast!", Toast.LENGTH_LONG).show();
             }
         });
     }
