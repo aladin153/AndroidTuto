6cbf256b18d6dda9e7bd40cf71ba7c9f378277fd
Alaeddine ZAYEN <alaedine.zayen@gmail.com>
2024-08-19 00:05:59 +0200

    RadioButtons
    Signed-off-by: Alaeddine ZAYEN <alaedine.zayen@gmail.com>

--- app/src/main/java/com/zayen/androidtuto/MainActivity.java
+++ app/src/main/java/com/zayen/androidtuto/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -26,12 +28,42 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radio_group);
+        radioGroup.clearCheck();
+        // Add the Listener to the RadioGroup
+        radioGroup.setOnCheckedChangeListener(
+                new RadioGroup.OnCheckedChangeListener() {
+                    @Override
+                    // The flow will come here when
+                    // any of the radio buttons in the radioGroup
+                    // has been clicked
+                    // Check which radio button has been clicked
+                    public void onCheckedChanged(RadioGroup group,
+                                                 int checkedId)
+                    {
+                        // Get the selected Radio Button
+                        RadioButton radioButton = (RadioButton)group.findViewById(checkedId);
+                        Toast.makeText(MainActivity.this, radioButton.getText() + " clicked ", Toast.LENGTH_LONG).show();
+                    }
+                });
+
         Button btn = (Button) findViewById(R.id.btn_test);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d(TAG, "Button Clicked");
-                Toast.makeText(MainActivity.this, "This this a Toast!", Toast.LENGTH_LONG).show();
+                // Get the Radio Button which is set
+                // If no Radio Button is set, -1 will be returned
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+                if (selectedId == -1) {
+                    Toast.makeText(MainActivity.this, "No answer has been selected", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    RadioButton radioButton = (RadioButton)radioGroup.findViewById(selectedId);
+                    // Now display the value of selected item
+                    // by the Toast message
+                    Toast.makeText(MainActivity.this, radioButton.getText(), Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
