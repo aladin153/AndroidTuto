f00324ab1d2f37830bbe3dd67fd163cf64b4e9c1
Alaeddine ZAYEN <alaedine.zayen@gmail.com>
2024-08-19 12:39:31 +0200

    Alert Dialog
    Signed-off-by: Alaeddine ZAYEN <alaedine.zayen@gmail.com>

--- app/src/main/java/com/zayen/androidtuto/MainActivity.java
+++ app/src/main/java/com/zayen/androidtuto/MainActivity.java
@@ -1,5 +1,7 @@
 package com.zayen.androidtuto;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -34,16 +36,32 @@ public class MainActivity extends AppCompatActivity {
         });
 
         Button btn = (Button) findViewById(R.id.btn_test);
-        CheckBox cb = (CheckBox) findViewById(R.id.checkBox);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (cb.isChecked()) {
-                    Toast.makeText(getApplicationContext(), "Checked", Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(getApplicationContext(), "Not Checked", Toast.LENGTH_LONG).show();
-                }
                 Log.d(TAG, "Button Clicked");
+                // Create the object of AlertDialog Builder class
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                // Set the message show for the Alert time
+                builder.setMessage("Do you want to exit ?");
+                // Set Alert Title
+                builder.setTitle("Alert !");
+                // Set Cancelable false for when the user clicks on the outside the Dialog Box then it will remain show
+                builder.setCancelable(false);
+                // Set the positive button with yes name Lambda OnClickListener method is use of DialogInterface interface.
+                builder.setPositiveButton("Yes", (DialogInterface.OnClickListener) (dialog, which) -> {
+                    // When the user click yes button then app will close
+                    finish();
+                });
+                // Set the Negative button with No name Lambda OnClickListener method is use of DialogInterface interface.
+                builder.setNegativeButton("No", (DialogInterface.OnClickListener) (dialog, which) -> {
+                    // If user click no then dialog box is canceled.
+                    dialog.cancel();
+                });
+                // Create the Alert dialog
+                AlertDialog alertDialog = builder.create();
+                // Show the Alert Dialog box
+                alertDialog.show();
             }
         });
     }
